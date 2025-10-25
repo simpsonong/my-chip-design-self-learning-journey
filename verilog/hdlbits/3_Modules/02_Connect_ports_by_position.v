4c674f1e9e17c5a3c8c1f53c64aebd7470c5455a
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 01:58:32 +0800

    Update 02_Connect_ports_by_position.v

---
+++ verilog/hdlbits/3_Modules/02_Connect_ports_by_position.v
@@ -0,0 +1,13 @@
+module top_module ( 
+    input a, 
+    input b, 
+    input c,
+    input d,
+    output out1,
+    output out2
+);
+    mod_a in1(out1,out2,a,b,c,d);
+endmodule
+
+// helpful short video for explanation (start from 0:52) :)
+// https://www.youtube.com/watch?v=0vtuCkwd-Tc&list=PLoZS1MH9uqWDYCf6fHLchYS2r1bQFcv3d&index=3
