7ee5b3bf752bc196725a86483a9173a5c44874cc
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:07:00 +0800

    Update 06_And_Gate.v

--- verilog/hdlbits/1_Basics/06_And_Gate.v
+++ verilog/hdlbits/1_Basics/06_And_Gate.v
@@ -2,8 +2,10 @@ module top_module(
     input a, 
     input b, 
     output out );
-
+assign out= a&b;
 endmodule
+
 
 // helpful short video for explanation(13min) :)
 // https://www.youtube.com/watch?v=NkqrOtGKR-A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=2
+
