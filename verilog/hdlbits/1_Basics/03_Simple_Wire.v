cfe5d94e55b958d3020331d23d40bae627a9a104
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:03:30 +0800

    Update 03_Simple_Wire.v

--- verilog/hdlbits/1_Basics/03_Simple_Wire.v
+++ verilog/hdlbits/1_Basics/03_Simple_Wire.v
@@ -2,5 +2,6 @@ module top_module( input in, output out );
 assign out=in;
 endmodule
 
-// helpful short video for explanation(7min) :)
+// helpful short video for explanation :)
 // https://www.youtube.com/watch?v=NkqrOtGKR-A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=2
+
