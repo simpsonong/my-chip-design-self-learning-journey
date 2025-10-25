97a78f9aea7c1165914e16082a828373bec684f4
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:05:00 +0800

    Update 05_Not_Gate.v

---
+++ verilog/hdlbits/1_Basics/05_Not_Gate.v
@@ -0,0 +1,8 @@
+module top_module( input in, output out );
+
+    assign out=~in;
+    
+endmodule
+
+// helpful short video for explanation(13min) :)
+// https://www.youtube.com/watch?v=NkqrOtGKR-A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=2
