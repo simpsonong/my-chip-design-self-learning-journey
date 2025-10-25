8780566ccd561dd527cdd835ccf955bfdf37b2d6
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:36:05 +0800

    Refactor comments in 02_Output_Zero.v
    Updated comments for clarity and consistency.

--- verilog/hdlbits/1_Basics/02_Output_Zero.v
+++ verilog/hdlbits/1_Basics/02_Output_Zero.v
@@ -4,6 +4,7 @@ module top_module(
 assign zero=1'b0;
 endmodule
 
-// helpful short video for explanation :)
+// helpful short video for explanation(start from 11:07) :)
 // https://www.youtube.com/watch?v=t27AV5XQrGE&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=1
+
 
