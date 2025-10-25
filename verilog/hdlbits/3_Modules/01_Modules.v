8506eb800fcaabe14b3f77fb5a09169f8c519203
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 01:54:02 +0800

    Update 01_Modules.v

---
+++ verilog/hdlbits/3_Modules/01_Modules.v
@@ -0,0 +1,7 @@
+module top_module ( input a, input b, output out );
+
+    mod_a in1(.out(out),.in1(a),.in2(b));
+endmodule
+
+// helpful short video for explanation (start from 1:06):)
+// https://www.youtube.com/watch?v=zLzFexW_cjc&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=7
