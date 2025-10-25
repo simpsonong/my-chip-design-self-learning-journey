c5f5fa63f719ac469693f30ad8422ea9a595dc8d
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:02:20 +0800

    Update 04_Four_Wires.v

---
+++ verilog/hdlbits/1_Basics/04_Four_Wires.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+    assign w=a;
+    assign x=b;
+    assign y=b;
+    assign z=c;
+endmodule
+
+// helpful short video for explanation :)
+// https://www.youtube.com/watch?v=NkqrOtGKR-A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=2
