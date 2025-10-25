c4cb6900ba22a9ca0900f6de96fcc44621625493
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:25:18 +0800

    Add top_module with wire declarations and assignments

---
+++ verilog/hdlbits/1_Basics/09_Declaring_Wires.v
@@ -0,0 +1,22 @@
+`default_nettype none
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    
+wire out_ab;
+    assign out_ab=a&b;
+    
+wire out_cd;
+    assign out_cd=c&d;
+    
+assign out=out_ab|out_cd;
+assign out_n=~out;  
+    
+endmodule
+
+// helpful short video for explanation :)
+// https://www.youtube.com/watch?v=1rUEctIsPlg&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=3
