e227b52a357fd4caecba9182a3a29e768544b35e
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:18:05 +0800

    Add XNOR gate module in Verilog
    Implement XNOR gate functionality in Verilog.

---
+++ verilog/hdlbits/1_Basics/08_XNOR_Gate.v
@@ -0,0 +1,9 @@
+module top_module( 
+    input a, 
+    input b, 
+    output out );
+assign out=~a^b;
+endmodule
+
+// helpful short video for explanation :)
+// https://www.youtube.com/watch?v=PcNcKJvtB10&t=30s
