525d13c95c6b4339666a0a2bd4c627f931fcee03
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 01:34:26 +0800

    Update 04_Bitwise_Operators.v

---
+++ verilog/hdlbits/2_Vectors/04_Bitwise_Operators.v
@@ -0,0 +1,24 @@
+module top_module( 
+    input [2:0] a,
+    input [2:0] b,
+    output [2:0] out_or_bitwise,
+    output out_or_logical,
+    output [5:0] out_not
+);
+    assign out_or_bitwise[0]=a[0]|b[0];
+    assign out_or_bitwise[1]=a[1]|b[1];
+    assign out_or_bitwise[2]=a[2]|b[2];
+    
+    assign out_or_logical=a||b;
+    
+    assign out_not[0]=~a[0];
+    assign out_not[1]=~a[1];
+    assign out_not[2]=~a[2];
+    assign out_not[3]=~b[0];
+    assign out_not[4]=~b[1];
+    assign out_not[5]=~b[2];
+
+endmodule
+
+// helpful short video for explanation(start from 6:11) :)
+// https://www.youtube.com/watch?v=LI5N_0dtQ9A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=5
