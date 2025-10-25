564e1dc47a65d406b1542e36b869d6365b3c5187
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 01:27:09 +0800

    Update 02_Vectors_in_more_detail.v

---
+++ verilog/hdlbits/2_Vectors/02_Vectors_in_more_detail.v
@@ -0,0 +1,13 @@
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+    
+    assign out_hi=in[15:8];
+    assign out_lo=in[7:0];
+    
+endmodule
+
+// helpful short video for explanation (6:41):)
+// https://www.youtube.com/watch?v=eA4O2BbNW2s&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=4
