1d6a993096a266c5d9f32686c4a423c2e13371f4
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:19:02 +0800

    Update 06_And_Gate.v

--- verilog/hdlbits/1_Basics/06_And_Gate.v
+++ verilog/hdlbits/1_Basics/06_And_Gate.v
@@ -6,6 +6,7 @@ assign out= a&b;
 endmodule
 
 
-// helpful short video for explanation(13min) :)
+// helpful short video for explanation :)
 // https://www.youtube.com/watch?v=NkqrOtGKR-A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=2
+
 
