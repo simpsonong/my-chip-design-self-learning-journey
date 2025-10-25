5fd7af8c787e1c5ab49c719e516d7989b6bddc12
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:03:10 +0800

    Update 02_Output_Zero.v

--- verilog/hdlbits/1_Basics/02_Output_Zero.v
+++ verilog/hdlbits/1_Basics/02_Output_Zero.v
@@ -4,5 +4,6 @@ module top_module(
 assign zero=1'b0;
 endmodule
 
-// helpful short video for explanation(13min) :)
+// helpful short video for explanation :)
 // https://www.youtube.com/watch?v=t27AV5XQrGE&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=1
+
