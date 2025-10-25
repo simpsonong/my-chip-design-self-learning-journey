d662a95b963b12e4e02a2301a716f1c951c32346
simpsonong <152422248+simpsonong@users.noreply.github.com>
2025-10-26 00:19:26 +0800

    Format and clean up Nor gate module code

--- verilog/hdlbits/1_Basics/07_Nor_Gate.v
+++ verilog/hdlbits/1_Basics/07_Nor_Gate.v
@@ -5,5 +5,6 @@ module top_module(
     assign out=~(a|b);
 endmodule
 
-// helpful short video for explanation(13min) :)
+// helpful short video for explanation :)
 // https://www.youtube.com/watch?v=NkqrOtGKR-A&list=PL0E9jhuDlj9qxAfV9hFKNQeHLWimarJJm&index=2
+
